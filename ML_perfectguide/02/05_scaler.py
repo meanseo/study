1463ab4e9d602e5750eafcdf5789d86f66999e85
meanseo <chlalstjok97@gmail.com>
2022-09-06 19:45:27 +0900

    스케일링 유의점

--- ML_perfectguide/02/05_scaler.py
+++ ML_perfectguide/02/05_scaler.py
@@ -72,3 +72,45 @@ petal length (cm)    1.0
 petal width (cm)     1.0
 dtype: float64
 '''
+
+#####################################################
+# scaler시 fit(), transform() 유의점
+# fit() 메소드는 train data로만 학습 후 사용
+# test data도 학습 시키게 되면  스케일링 기준 정보가 달라지기 때문에 결과가 달라질 수 있음
+
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+# 학습 데이터는 0 부터 10까지, 테스트 데이터는 0 부터 5까지 값을 가지는 데이터 세트로 생성
+# Scaler클래스의 fit(), transform()은 2차원 이상 데이터만 가능하므로 reshape(-1, 1)로 차원 변경
+train_array = np.arange(0, 11).reshape(-1, 1)
+test_array =  np.arange(0, 6).reshape(-1, 1)
+
+# 최소값 0, 최대값 1로 변환하는 MinMaxScaler객체 생성
+scaler = MinMaxScaler()
+# fit()하게 되면 train_array 데이터의 최소값이 0, 최대값이 10으로 설정.  
+scaler.fit(train_array)
+# 1/10 scale로 train_array 데이터 변환함. 원본 10-> 1로 변환됨.
+train_scaled = scaler.transform(train_array)
+ 
+print('원본 train_array 데이터:', np.round(train_array.reshape(-1), 2))
+print('Scale된 train_array 데이터:', np.round(train_scaled.reshape(-1), 2))
+
+# 앞에서 생성한 MinMaxScaler에 test_array를 fit()하게 되면 원본 데이터의 최소값이 0, 최대값이 5으로 설정됨 
+scaler.fit(test_array)
+# 1/5 scale로 test_array 데이터 변환함. 원본 5->1로 변환.  
+test_scaled = scaler.transform(test_array)
+# train_array 변환 출력
+print('원본 test_array 데이터:', np.round(test_array.reshape(-1), 2))
+print('Scale된 test_array 데이터:', np.round(test_scaled.reshape(-1), 2))
+
+scaler = MinMaxScaler()
+scaler.fit(train_array)
+train_scaled = scaler.transform(train_array)
+print('원본 train_array 데이터:', np.round(train_array.reshape(-1), 2))
+print('Scale된 train_array 데이터:', np.round(train_scaled.reshape(-1), 2))
+
+# test_array에 Scale 변환을 할 때는 반드시 fit()을 호출하지 않고 transform() 만으로 변환해야 함. 
+test_scaled = scaler.transform(test_array)
+print('\n원본 test_array 데이터:', np.round(test_array.reshape(-1), 2))
+print('Scale된 test_array 데이터:', np.round(test_scaled.reshape(-1), 2))

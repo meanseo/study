24e8b9be76d130d353f191bf20ca80657cf64e75
meanseo <chlalstjok97@gmail.com>
2022-09-14 22:16:24 +0900

    LinearRegression

---
+++ ML_perfectguide/05/04_linearRegression_boston.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error , r2_score
+from sklearn.model_selection import cross_val_score
+
+# boston 데이타셋 로드
+boston = load_boston()
+
+# boston 데이타셋 DataFrame 변환 
+bostonDF = pd.DataFrame(boston.data , columns = boston.feature_names)
+
+# boston dataset의 target array는 주택 가격임. 이를 PRICE 컬럼으로 DataFrame에 추가함. 
+bostonDF['PRICE'] = boston.target
+print('Boston 데이타셋 크기 :',bostonDF.shape)
+bostonDF.head()
+
+# 2개의 행과 4개의 열을 가진 subplots를 이용. axs는 4x2개의 ax를 가짐.
+fig, axs = plt.subplots(figsize=(16,8) , ncols=4 , nrows=2)
+lm_features = ['RM','ZN','INDUS','NOX','AGE','PTRATIO','LSTAT','RAD']
+for i , feature in enumerate(lm_features):
+    row = int(i/4)
+    col = i%4
+    # 시본의 regplot을 이용해 산점도와 선형 회귀 직선을 함께 표현
+    sns.regplot(x=feature , y='PRICE',data=bostonDF , ax=axs[row][col])
+    
+y_target = bostonDF['PRICE']
+X_data = bostonDF.drop(['PRICE'],axis=1,inplace=False)
+
+X_train , X_test , y_train , y_test = train_test_split(X_data , y_target ,test_size=0.3, random_state=156)
+
+# Linear Regression OLS로 학습/예측/평가 수행. 
+lr = LinearRegression()
+lr.fit(X_train ,y_train )
+y_preds = lr.predict(X_test)
+mse = mean_squared_error(y_test, y_preds)
+rmse = np.sqrt(mse)
+
+print('MSE : {0:.3f} , RMSE : {1:.3F}'.format(mse , rmse))
+print('Variance score : {0:.3f}'.format(r2_score(y_test, y_preds)))
+
+# 회귀 계수를 큰 값 순으로 정렬하기 위해 Series로 생성. index가 컬럼명에 유의
+coeff = pd.Series(data=np.round(lr.coef_, 1), index=X_data.columns )
+coeff.sort_values(ascending=False)
+
+y_target = bostonDF['PRICE']
+X_data = bostonDF.drop(['PRICE'],axis=1,inplace=False)
+lr = LinearRegression()
+
+# cross_val_score( )로 5 Fold 셋으로 MSE 를 구한 뒤 이를 기반으로 다시  RMSE 구함. 
+neg_mse_scores = cross_val_score(lr, X_data, y_target, scoring="neg_mean_squared_error", cv = 5)
+rmse_scores  = np.sqrt(-1 * neg_mse_scores)
+avg_rmse = np.mean(rmse_scores)
+
+# cross_val_score(scoring="neg_mean_squared_error")로 반환된 값은 모두 음수 
+print(' 5 folds 의 개별 Negative MSE scores: ', np.round(neg_mse_scores, 2))
+print(' 5 folds 의 개별 RMSE scores : ', np.round(rmse_scores, 2))
+print(' 5 folds 의 평균 RMSE : {0:.3f} '.format(avg_rmse))
